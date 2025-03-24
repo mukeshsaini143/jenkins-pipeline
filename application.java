044396c23b95ff17bdf43dcb6546834616d72e29
mukeshsaini143 <94359081+mukeshsaini143@users.noreply.github.com>
2025-03-24 13:43:04 +0530

    Update application.java

--- application.java
+++ application.java
@@ -1,6 +1,6 @@
-public class application{
+public class Application{
 
   public static void main(string[]args){
-    System.out.println("Hello World!")  
+    System.out.println("Hello World!"); 
   }
 }
